9c8758e56b2eb1268a70b8dd795d7c15b6b60039
Naresh Khatri <68143538+nareshKhatri-025@users.noreply.github.com>
2020-10-18 16:34:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,30 +2,62 @@ $(document).ready(function(){
     $('.menu-toggler').on("click", function(){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
-
     });
 
     $('.top-nav .nav-link').on("click", function(){
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
-
     });
 
     $('nav a[href*="#"]').on("click", function(){
-
         $('html, body').animate({
             scrollTop:$($(this).attr('href')).offset().top - 100
         },2000); 
     });
     $('#up').on("click", function(){
-
         $('html, body').animate({
             scrollTop:0
         },2000);
     });
 
-    AOS.init({
-        easing:'ease',
-        duration: 1800
+    // init controller
+    var controller = new ScrollMagic.Controller();
+
+    // create a scene
+    let profileScene = new ScrollMagic.Scene({
+        triggerElement: '.profile-img',
+    }).setClassToggle('.profile-img', 'fade-left').addTo(controller);
+
+    let aboutDetailsScene = new ScrollMagic.Scene({
+        triggerElement: '.about-details',
+    }).setClassToggle('.about-details', 'fade-right').addTo(controller);
+
+    let skills = document.querySelectorAll(".skill");
+
+    skills.forEach((skill, index) => {
+        let skillScene = new ScrollMagic.Scene({
+            triggerElement: skill,
+            offset: 50 + (index * 50),
+        }).setClassToggle(skill, 'show-skill').addTo(controller);
     });
+
+    let projectImgScene = new ScrollMagic.Scene({
+        triggerElement: '.projects-img',
+    }).setClassToggle('.projects-img', 'fade-left').addTo(controller);
+
+    let projectDesScene = new ScrollMagic.Scene({
+        triggerElement: '.project-description',
+    }).setClassToggle('.project-description', 'fade-left').addTo(controller);
+
+
+    let projectMidImgScene = new ScrollMagic.Scene({
+        triggerElement: '.projects-img-mid',
+    }).setClassToggle('.projects-img-mid', 'fade-right').addTo(controller);
+
+    let projectMidDesScene = new ScrollMagic.Scene({
+        triggerElement: '.project-description-mid',
+    }).setClassToggle('.project-description-mid', 'fade-right').addTo(controller);
+
+
+
 });
